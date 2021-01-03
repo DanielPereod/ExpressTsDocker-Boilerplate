1180056c6f7736d3cbaeaa81bc42aa5910e4379d
Daniel <danielpereod@gmail.com>
2021-01-03 15:50:39 +0000

    Server run string changed

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import App from "./app";
 const PORT = process.env.SERVER_PORT;
 
 App.app.listen(PORT, () => {
-  console.log(`🛡️  Server listening on port: http://localhost:${PORT} 🛡️`);
+  console.log(` Server listening on port: http://localhost:${PORT}`);
 });
